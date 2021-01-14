87ddeb5ff1d9527b19fe7907f96d72333ed04046
Charles Tanguy <charles.tanguy2@gmail.com>
2021-01-14 14:09:36 +0100

    Update app.py

--- docker/cake_app/app.py
+++ docker/cake_app/app.py
@@ -7,7 +7,7 @@ def main():
     print(f"And I like {os.environ['MY_CAKE']} too!")
     print("i'm on a new branch!")
     print("solve example issue")
-    
+    print("test git status")
 
 if __name__ == "__main__":
     main()
